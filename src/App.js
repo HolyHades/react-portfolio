b7fd75fe15f2020c981b3284678dad2b87698808
Felix Fröhlich <33263529+HolyHades@users.noreply.github.com>
2020-08-06 19:50:08 +0200

    yeet

--- src/App.js
+++ src/App.js
@@ -48,6 +48,10 @@ export default function App() {
         <Route path="/" exact component={Home} />
         <Route path="/about"  component={About} />
         <Route path="/gallery"  component={Gallery} />
+        <Route path="/thankyou" component={success} />
+        </AnimatedSwitch>
+      </Switch>
+      <Switch>
         <Route path="/image1" component={image1} />
         <Route path="/image2" component={image2} />
         <Route path="/image3" component={image3} />
@@ -55,8 +59,6 @@ export default function App() {
         <Route path="/image5" component={image5} />
         <Route path="/image6" component={image6} />
         <Route path="/image7" component={image7} />
-        <Route path="/thankyou" component={success} />
-        </AnimatedSwitch>
       </Switch>
     </div>
     </Router>
